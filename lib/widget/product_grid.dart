7a8884e0cd980fe14cae6489c076128a8a82a4ac
utkc137-codeverse <utkarsh.learn2020@gmail.com>
2020-07-10 20:40:23 +0530

    #

--- lib/widget/product_grid.dart
+++ lib/widget/product_grid.dart
@@ -11,8 +11,8 @@ class ProductGrid extends StatelessWidget{
     final products =productData.items;
     return GridView.builder(
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, childAspectRatio: 3/2 , crossAxisSpacing: 10, mainAxisSpacing: 10), 
-      itemBuilder: (ctx, i) => ChangeNotifierProvider( 
-        create: (ctx) => products[i],
+      itemBuilder: (ctx, i) => ChangeNotifierProvider.value( 
+        value: products[i],
         child:ProductItem()),
       padding: const EdgeInsets.all(10),
       itemCount: products.length,
